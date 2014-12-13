7b3b929763ac5e7a3b4df33bf2e4374c944036f7
dmzx <info@dmzx-web.net>
2014-12-13 20:14:46 +0100

    Spanish translation
    Spanish translation

---
+++ language/es/common.php
@@ -0,0 +1,29 @@
+<?php
+/*
+*
+* @package Pretty topic
+* @author dmzx (www.dmzx-web.net)
+* @copyright (c) 2014 by dmzx (www.dmzx-web.net)
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* 
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'VIEW_TOPIC_ANNOUNCEMENT'   => 'Anuncio',
+    'VIEW_TOPIC_GLOBAL'         => 'Anuncio G.',
+    'VIEW_TOPIC_LOCKED'         => 'Cerrado',
+    'VIEW_TOPIC_LOGS'           => 'Ver regstros',
+    'VIEW_TOPIC_MOVED'          => 'Movido',
+    'VIEW_TOPIC_POLL'           => 'Encuesta',
+    'VIEW_TOPIC_STICKY'         => 'Nota',
+));
