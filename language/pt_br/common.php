0ee04cb6748488916e156c759bdbe41701b2ccab
dmzx <info@dmzx-web.net>
2015-07-12 13:28:04 +0200

    update

--- language/pt_br/common.php
+++ language/pt_br/common.php
@@ -3,10 +3,10 @@
 *
 * @package Pretty topic
 * @author dmzx (www.dmzx-web.net)
-* Brazilian Portuguese  translation by MarcosFPo
+* Brazilian Portuguese	translation by MarcosFPo
 * @copyright (c) 2014 by dmzx (www.dmzx-web.net)
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
-* 
+*
 */
 
 if (!defined('IN_PHPBB'))
@@ -20,11 +20,11 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-    'VIEW_TOPIC_ANNOUNCEMENT'   => 'Anúncio',
-    'VIEW_TOPIC_GLOBAL'         => 'Anúncio Global',
-    'VIEW_TOPIC_LOCKED'         => 'Trancado',
-    'VIEW_TOPIC_LOGS'           => 'Ver logs',
-    'VIEW_TOPIC_MOVED'          => 'Movido',
-    'VIEW_TOPIC_POLL'           => 'Enquete',
-    'VIEW_TOPIC_STICKY'         => 'Fixado',
+	'VIEW_TOPIC_ANNOUNCEMENT'	=> 'Anúncio',
+	'VIEW_TOPIC_GLOBAL'		 => 'Anúncio Global',
+	'VIEW_TOPIC_LOCKED'		 => 'Trancado',
+	'VIEW_TOPIC_LOGS'			=> 'Ver logs',
+	'VIEW_TOPIC_MOVED'			=> 'Movido',
+	'VIEW_TOPIC_POLL'			=> 'Enquete',
+	'VIEW_TOPIC_STICKY'		 => 'Fixado',
 ));
